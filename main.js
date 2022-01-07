0e468d368fac0e2a8d0ff744ec3c5d59b9caecff
Manon Oriol <oriol.manon@gmail.com>
2022-01-07 19:36:42 +0100

    L5 completed

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ const app = Vue.createApp({
     data: function() {
         return {
             product: 'Socks',
-            description : '100% wool and handmade in France',
-            image : './assets/images/socks_green.jpg',
-            url : 'https://s3.eu-west-2.amazonaws.com/craft-world/cms/content/2021/04/pileofsocks.png',
-            inventory : 8,
-            onSale : true
+            description: 'Handmade in France',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://s3.eu-west-2.amazonaws.com/craft-world/cms/content/2021/04/pileofsocks.png',
+            inventory: 8,
+            onSale: false,
+            details: ['70% cotton', '20% wool', '10% bamboo fibers'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: ['XS', 'S', 'M', 'L']
         }
     }
 })
